057cee39d87f43c25aec305f4f5cbc52f367c955
suh1z <jobo.holmberg@gmail.com>
2024-07-30 02:01:12 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const testingRouter = require("./controllers/testing");
 
 mongoose.set("strictQuery", false);
 
-logger.info("connecting to", config.MONGODB_URI);
+logger.info("connecting to", config.MONGODB_URI)
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -24,10 +24,10 @@ mongoose
     logger.error("error connection to MongoDB:", error.message);
   });
 
-app.use(cors());
-app.use(express.static("dist"));
-app.use(express.json());
 app.use(middleware.requestLogger);
+app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
 
 app.use("/api/login", loginRouter);
 app.use("/api/users", usersRouter);
