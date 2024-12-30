d814e1bd3c95289659becf88d3d79c53d43f1a77
suh1z <jobo.holmberg@gmail.com>
2024-12-30 14:44:12 +0200

    graph

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -79,7 +79,7 @@ const Navbar = (props) => {
           <>
             <Box sx={{ display: 'flex', ml: 'auto' }}>
               <Link to="/" className="link">
-                Loveetify
+                LoveCanal
               </Link>
               <Link to="/playerdata" className="link">
                 Player Data
