2f2edb1beca7d218293571a73126b1377327b545
suh1z <jobo.holmberg@gmail.com>
2026-02-08 13:25:44 +0200

    Remove dist from git

--- dist/assets/BarChart-CLlbfNDE.js
+++
@@ -1,1 +0,0 @@
-import{l as a,B as i,X as s,Y as t,m as e}from"./generateCategoricalChart-YPdEnS0l.js";var x=a({chartName:"BarChart",GraphicalChild:i,defaultTooltipEventType:"axis",validateTooltipEventTypes:["axis","item"],axisComponents:[{axisType:"xAxis",AxisComp:s},{axisType:"yAxis",AxisComp:t}],formatAxisMap:e});export{x as B};
--- dist/assets/BarChart-CPJJ1i88.js
+++
@@ -1,1 +0,0 @@
-import{g as a,B as i,X as s,Y as t,f as e}from"./generateCategoricalChart-PJmzUER8.js";var x=a({chartName:"BarChart",GraphicalChild:i,defaultTooltipEventType:"axis",validateTooltipEventTypes:["axis","item"],axisComponents:[{axisType:"xAxis",AxisComp:s},{axisType:"yAxis",AxisComp:t}],formatAxisMap:e});export{x as B};
